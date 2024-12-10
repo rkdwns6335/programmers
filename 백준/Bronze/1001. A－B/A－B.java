e2a904da2c5ec5e87b3c834ca21a05e87d8b2b0e
KangJun <76244088+rkdwns6335@users.noreply.github.com>
2024-12-10 10:31:49 +0900

    [Bronze V] Title: A-B, Time: 176 ms, Memory: 17704 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a-b);
+
+    }
+}
