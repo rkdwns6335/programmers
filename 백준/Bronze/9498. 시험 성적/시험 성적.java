0e47861e72bf03f745186569af8f9de2b2fe5a2d
KangJun <76244088+rkdwns6335@users.noreply.github.com>
2024-12-10 10:40:40 +0900

    [Bronze V] Title: 시험 성적, Time: 172 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int score = scan.nextInt();
+        
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}
