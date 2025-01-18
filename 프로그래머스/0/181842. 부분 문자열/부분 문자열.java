06b6d3661b9f69579f113579f62f073e683636b6
KangJun <76244088+rkdwns6335@users.noreply.github.com>
2025-01-18 18:33:49 +0900

    Update 부분 문자열.java

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(String str1, String str2) {
         int answer = 0;
-        answer = str2.contains(str1) ? 1 : 0;
+        answer = str2.contains(str1) ? 1 : 0; // contains 함수를 활용해 문자열이 특정 문자열을 포함하고 있는지 확인
         return answer;
     }
 }
