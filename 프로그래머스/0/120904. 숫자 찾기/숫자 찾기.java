de3af4f7079b9f00be8f0652d0403387388ad565
KangJun <76244088+rkdwns6335@users.noreply.github.com>
2025-02-26 16:43:56 +0900

    [level 0] Title: 숫자 찾기, Time: 0.03 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120904. 숫자 찾기/숫자 찾기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int num, int k) {
+        int answer = -1;
+		String numStr = String.valueOf(num);
+
+		for(int i=0; i<numStr.length(); i++) {
+			if(numStr.charAt(i) == (char) (k + '0')) {
+				answer = i+1;
+				break;
+			}
+		}
+        return answer;
+    }
+}
